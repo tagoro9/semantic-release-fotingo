e76d6701e85002b39bbde15ce84edccf78d5d209
Victor <victor@turo.com>
2021-03-11 19:09:39 -0800

    feat: specify release version and add logs

--- src/success.ts
+++ src/success.ts
@@ -3,5 +3,14 @@ import { Context } from "semantic-release";
 import { callFotingo } from "~/callFotingo";
 
 export async function success(_: Record<string, unknown>, context: Context): Promise<void> {
-  await callFotingo(["release", "-n"], context.logger, { env: context.env });
+  if (context.options?.dryRun) {
+    console.log(JSON.stringify(context, undefined, 2));
+    context.logger.log("Skipping fotingo release. Dry run");
+    return;
+  }
+  if (!context.nextRelease) {
+    context.logger.error("Could not find next release. Exiting");
+    return;
+  }
+  await callFotingo(["release", "-n", context.nextRelease.version], context.logger, { env: context.env });
 }
--- src/verifyConditions.ts
+++ src/verifyConditions.ts
@@ -3,5 +3,6 @@ import { Context } from "semantic-release";
 import { callFotingo } from "~/callFotingo";
 
 export async function verifyConditions(_: Record<string, unknown>, context: Context): Promise<void> {
+  console.log(JSON.stringify(context, undefined, 2));
   await callFotingo(["verify"], context.logger, { env: context.env });
 }
