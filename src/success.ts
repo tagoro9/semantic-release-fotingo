b225d5c1e050aaf6cfde8291778c97f98f08d101
Victor <victor@turo.com>
2021-03-23 14:28:36 -0700

    feat: pass fixed issues to fotingo

--- src/success.ts
+++ src/success.ts
@@ -1,6 +1,24 @@
-import { Context } from "semantic-release";
+import { sync } from "conventional-commits-parser";
+import { Context, Result } from "semantic-release";
 
 import { callFotingo } from "~/callFotingo";
+
+/**
+ * Given the release context, return the list of issues that were fixed so they
+ * can be passed to fotingo
+ * @param context Release context
+ * @returns List of fixes issues in the release
+ */
+function getIssuesInRelease(context: Result | Context): string[] {
+  if (typeof context === "object" && "commits" in context) {
+    return context.commits
+      .map((commit) => sync(commit.message).references)
+      .flat()
+      .filter((reference) => reference.action && /fixes/i.test(reference.action))
+      .map((reference) => reference.issue);
+  }
+  return [];
+}
 
 export async function success(_: Record<string, unknown>, context: Context): Promise<void> {
   if (context.options?.dryRun) {
@@ -12,5 +30,6 @@ export async function success(_: Record<string, unknown>, context: Context): Pro
     context.logger.error("Could not find next release. Exiting");
     return;
   }
-  await callFotingo(["release", "-n", context.nextRelease.version], context.logger, { env: context.env });
+  const issues = getIssuesInRelease(context).map((issue) => `-i ${issue}`);
+  await callFotingo(["release", "-n", context.nextRelease.version, ...issues], context.logger, { env: context.env });
 }
