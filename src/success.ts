3c8a75966cbc5a4b095d584642bc614da04b2cd2
Victor <victor@turo.com>
2021-03-23 14:51:21 -0700

    fix: remove logs with circular structures

--- src/success.ts
+++ src/success.ts
@@ -22,7 +22,6 @@ function getIssuesInRelease(context: Result | Context): string[] {
 
 export async function success(_: Record<string, unknown>, context: Context): Promise<void> {
   if (context.options?.dryRun) {
-    console.log(JSON.stringify(context, undefined, 2));
     context.logger.log("Skipping fotingo release. Dry run");
     return;
   }
--- src/verifyConditions.ts
+++ src/verifyConditions.ts
@@ -3,6 +3,5 @@ import { Context } from "semantic-release";
 import { callFotingo } from "~/callFotingo";
 
 export async function verifyConditions(_: Record<string, unknown>, context: Context): Promise<void> {
-  console.log(JSON.stringify(context, undefined, 2));
   await callFotingo(["verify"], context.logger, { env: context.env });
 }
