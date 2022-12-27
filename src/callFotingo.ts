5f136f3f4f8c85749e2a516a272a9b15b87109b8
Victor <victor@turo.com>
2022-12-27 18:04:50 +0000

    refactor: return stdout from fotingo calls

--- src/callFotingo.ts
+++ src/callFotingo.ts
@@ -18,8 +18,9 @@ export function callFotingo(
   arguments_: string | string[],
   context: Context,
   options?: { cwd?: string; env?: { [k: string]: string } }
-): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
+): Promise<string> {
+  const output: string[] = [];
+  return new Promise<string>((resolve, reject) => {
     const arguments__ = Array.isArray(arguments_) ? arguments_ : [arguments_];
     debug(`running fotingo release with args: ${arguments__}`);
     // eslint-disable-next-line unicorn/prefer-module
@@ -35,6 +36,7 @@ export function callFotingo(
       },
     });
     fotingoCmd.stdout.on("data", (data: string) => {
+      output.push(data);
       context.logger.log(data.toString());
     });
     fotingoCmd.stderr.on("data", (data: string) => {
@@ -47,7 +49,7 @@ export function callFotingo(
     fotingoCmd.on("close", (code: number) => {
       debug("fotingo command exited with code %s", code);
       if (code === 0) {
-        resolve();
+        resolve(output.join(""));
       } else {
         reject(new Error(`Fotingo exited with code ${code}`));
       }
